0c976c43be0d4b9ea6827e77cf0af78ad8a948ee
IsiOgas <isidoraogaspavez@gmail.com>
2025-06-22 23:46:13 -0400

    se agregan directrices

--- prueba.cpp
+++ prueba.cpp
@@ -31,15 +31,40 @@ class TDAGrafo {
         }
     }
 
-    //Agrega un arco entre dos nodos.
+    /* ****
+    * Tipo Función: void agregarArco
+    ******
+    * Resumen Función: Agrega un arco desde un nodo origen
+    * hacia un nodo "destino" en la lista de adyacencia (ListaVecinoAdyacente).
+    ******
+    * Input :
+    * int origen: Índice del nodo desde donde se origina el arco.
+    * int destino: Índice del nodo hacia donde apunta el arco.
+    ******
+    * Returns :
+    * No retorna nada, pero modifica la estructura ListaVecinoAdyacente.
+    **** */
+
     void agregarArco(int origen, int destino) {
         Nodo* nuevo = new Nodo;
         nuevo->destino = destino;
         nuevo->sig = ListaVecinoAdyacente[origen]; // Insertar al inicio.
         ListaVecinoAdyacente[origen] = nuevo;
-
     } 
     
+    /* ****
+    * Tipo Función: setConductores
+    ******
+    * Resumen Función: carga y almacena la lista de los confuctores disponibles.
+    ******
+    * Input :
+    * int* lista: Es un arreglo donde se guardan los conductores identificados.
+    * int cant: Son la cantidad de conductores en el arreglo (lista como anteriormente le pusimos).
+    ******
+    * Returns :
+    * No retorna nada, solo asigna memoria y copia la lista.
+    **** */
+   
     void setConductores(int* lista, int cant) {
         cantidadConductores = cant;
         conductores = new int[cant];
@@ -51,6 +76,21 @@ class TDAGrafo {
     int getCantidadConductores() const { return cantidadConductores; }
     int getConductor(int i) const { return conductores[i]; }
     void setConductor(int i, int nodo) { conductores[i] = nodo; }
+
+    /* ****
+    * Tipo Función: void BFS
+    ******
+    * Resumen Función: Realiza un recorrido en anchura desde el nodo de inicio. Calcula la distancia mínima
+    * en número de aristas desde el nodo de inicio a todos los demás, luego regustra el padre de cada nodo alcanzado.
+    ******
+    * Input :
+    * int inicio: Es el nodo en donde comienza el recorrido BFS.
+    * int* distancia: Arreglo en donde se guarda la distancia desde el nodo de inicio hasta cada nodo.
+    * int* padre: Arreglo en donde se indica desde que nodo se llego a cada nodo.
+    ******
+    * Returns :
+    * No retorna nada, solo modifica los arreglos "distancia" y "padre"
+    **** */
 
     //Como se ocupa una lista de ady, se visita el nodo una vez O(n) y las aristas se recorren una sola vez O(a). Complejidad O(n + a).
     void BFS(int inicio, int* distancia, int* padre) {
@@ -105,6 +145,21 @@ class TDAGrafo {
     }
 };
 
+/* ****
+    * Tipo Función: mostarCamino
+    ******
+    * Resumen Función: Imprime el camino desde el nodo de origen hasta el nodo de destino utilizando
+    * información del arreglo padre, y si no hay camino imprime "{}".
+    ******
+    * Input :
+    * int destino: Nodo destino del cual se reconstruye el arreglo.
+    * int* padre: Arreglo donde padre[i] indica el nodo anterior a i en el camino.
+    * int totalNodos: cantidad total de nodos en el grafo.
+    ******
+    * Returns :
+    * No retorna valores, imprime por pantalla el camino en orden correcto.
+    **** */
+
 //Toma O(n).
 void mostrarCamino(int destino, int* padre, int totalNodos) {
     int* camino = new int[totalNodos + 1];  //O(n).
@@ -134,12 +189,27 @@ void mostrarCamino(int destino, int* padre, int totalNodos) {
     delete[] camino;    //O(1).
 }
 
+/* ****
+    * Tipo Función: solicitarUber
+    ******
+    * Resumen Función: Busca el conductor más cercano al pasajero utilizando BFS. luego 
+    * calcula el camino desde el conductor al pasajero y desde el pasajero al destino, y si
+    * existe una ruta valida, imprime la ruta final y el costo total del viaje.
+    ******
+    * Input :
+    * TDAGrafo& grafo: Grafo que contiene los nodos, aristas y posiciones de los conductores.
+    * int pasajero: Nodo en donde se encuentran los pasajeros.
+    * int destino: Nodo al que desea llegar el pasajero.
+    ******
+    * Returns :
+    * No retorna valores, imprime la ruta y el costo por consola y actualiza la posición del 
+    * conductor seleccionando el grafo.
+    **** */
 
 void solicitarUber(TDAGrafo& grafo, int pasajero, int destino) {
     int cantidadNodos = grafo.getCantidadNodos();               //O(1).
     int cantidadConductores= grafo.getCantidadConductores();    //O(1).
 
-    cout << "Pasajero nodo: " << pasajero << " | Destino nodo: " << destino << endl;
     int mejor = -1;       //O(1).
     int menorDist = 1e9; //Inicialmente una cantidad muy grande. O(1)
 
@@ -158,7 +228,6 @@ void solicitarUber(TDAGrafo& grafo, int pasajero, int destino) {
     }//Como se repite i < cantidadConductores, O(c *(n+a)), c=cantidadConductores.
 
     if (mejor == -1){
-        cout << "No hay conductor disponible para llegar al pasajero." << endl;
         cout << "Ruta : {} - Costo : -1" << endl;
         return;
     }//O(1).
@@ -171,7 +240,6 @@ void solicitarUber(TDAGrafo& grafo, int pasajero, int destino) {
     grafo.BFS(posConductor, distConductor, padreConductor); //O(n+a).
 
     if (distConductor[pasajero] == NOVISITADO) {
-        cout << "Conductor no puede llegar al pasajero." << endl;
         cout << "Ruta : {} - Costo : -1" << endl;
         delete[] distConductor;
         delete[] padreConductor;
@@ -185,7 +253,6 @@ void solicitarUber(TDAGrafo& grafo, int pasajero, int destino) {
 
     //Si no hay un camino desde el pasajero al destino.
     if(distPasajero[destino] == NOVISITADO){
-        cout << "No hay ruta del pasajero al destino." << endl;
         cout << "Ruta : {} - Costo : -1" << endl;
         delete[] distConductor;
         delete[] padreConductor;
@@ -197,10 +264,6 @@ void solicitarUber(TDAGrafo& grafo, int pasajero, int destino) {
     cout << "Ruta : ";
     mostrarCamino(destino, padrePasajero, cantidadNodos);   //O(n).
     cout << " - ";
-
-    //Mostramos las distancias.
-    cout << "Pasos conductor -> pasajero: " << distConductor[pasajero] << endl;
-    cout << "Pasos pasajero -> destino: " << distPasajero[destino] << endl;
 
     //Calculamos el costo total.
     int costo = 300 * distConductor[pasajero] + 500 * distPasajero[destino];    //O(1).
@@ -229,10 +292,6 @@ int main() {
     int cantidadArcos; 
     int cantidadConductores;
     archivo >> cantidadNodos >> cantidadArcos >> cantidadConductores;
-    //BORRAR LUEGO
-    cout<< "Nodos: " << cantidadNodos <<endl;
-    cout<< "Arcos: "<< cantidadArcos << endl;
-    cout<< "Conductores: " << cantidadConductores << endl;
 
     //Crear grafo usando el TDA.
     TDAGrafo grafo(cantidadNodos);
